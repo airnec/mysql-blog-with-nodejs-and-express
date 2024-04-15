d546adbe14f89cd16bf8a61b90aa70eda00d1880
Necip Dirik <necipdirik@gmail.com>
2024-04-15 16:15:17 +0300

    fetching and displaying a single blog post added

--- routes/blog.js
+++ routes/blog.js
@@ -30,6 +30,22 @@ router.post('/posts', async function (req, res) {
   res.redirect('/posts');
 });
 
+router.get('/posts/:id', async function(req, res) {
+  const query = `
+  SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+  INNER JOIN authors ON posts.author_id = authors.id
+  WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  res.render('post-detail', {post: posts[0]});
+})
+
 router.get('/new-post', async function (req, res) {
   const [authors] = await db.query('SELECT * FROM authors');
   res.render('create-post', { authors: authors });
