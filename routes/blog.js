ee72bebf3ade5edd31722ac0fddd7a0d8f0c9cb2
Necip Dirik <necipdirik@gmail.com>
2024-04-15 13:30:39 +0300

    injecting dynamic data added

--- routes/blog.js
+++ routes/blog.js
@@ -4,17 +4,30 @@ const db = require('../data/database');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('posts')
+router.get('/', function (req, res) {
+  res.redirect('posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list')
+router.get('/posts', function (req, res) {
+  res.render('posts-list');
 });
 
-router.get('/new-post', async function(req, res) {
+router.post('/posts', async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [
+    data,
+  ]);
+  res.redirect('/posts');
+});
+
+router.get('/new-post', async function (req, res) {
   const [authors] = await db.query('SELECT * FROM authors');
-  res.render('create-post', {authors: authors})
+  res.render('create-post', { authors: authors });
 });
 
 module.exports = router;
